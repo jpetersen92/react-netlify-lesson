b19e714842a734bc72b8364d9a4800daa9605ecf
Joshua Petersen <joshpetersen@Joshuas-MacBook-Air.local>
2021-10-04 09:57:56 -0700

    I have no idea

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>My React Netlify Application</h1>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="game" className="game">
+      <div id="block"></div>
+        <div id="hole"></div>
+        <div id="character"></div>
     </div>
   );
 }
